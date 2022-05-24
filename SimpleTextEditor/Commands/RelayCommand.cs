c3aa3e20ed8aa79a84ccf202ba1737c298453f2f
Darya Akhromenko <otulissa2012@yandex.ru>
2022-05-24 20:17:21 +0700

    Базовая реализация команды

---
+++ SimpleTextEditor/Commands/RelayCommand.cs
@@ -0,0 +1,30 @@
+﻿using SimpleTextEditor.Commands.Base;
+using System;
+
+namespace SimpleTextEditor.Commands
+{
+    internal class RelayCommand : Command
+    {
+        private readonly Func<object, bool> _canExecute;
+
+        private readonly Action<object> _execute;
+
+        public RelayCommand(Action<object> execute, Func<object, bool> canExecute = null)
+        {
+            _execute = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecute = canExecute;
+        }
+
+        public override bool CanExecute(object parameter) => _canExecute?.Invoke(parameter) ?? true;
+
+        public override void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+            {
+                return;
+            }
+
+            _execute(parameter);
+        }
+    }
+}
