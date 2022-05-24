8dd378e6381b33da94cae7486ba696d2973ecfd1
Darya Akhromenko <otulissa2012@yandex.ru>
2022-05-24 19:59:58 +0700

    Модель представления главного окна

---
+++ SimpleTextEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,47 @@
+﻿using SimpleTextEditor.ViewModels.Base;
+using System.IO;
+
+namespace SimpleTextEditor.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        private string _text;
+
+        public string Text
+        {
+            get => _text;
+
+            set => Set(ref _text, value);
+        }
+
+        private string _fileName;
+
+        public string FileName
+        {
+            get => _fileName;
+
+            set
+            {
+                if (Set(ref _fileName, value))
+                {
+                    ReadFileAsync(value);
+                }
+            }
+        }
+
+        private async void ReadFileAsync(string filePath)
+        {
+            Text = string.Empty;
+
+            if (!File.Exists(filePath))
+            {
+                return;
+            }
+
+            using (StreamReader reader = File.OpenText(filePath))
+            {
+                Text = await reader.ReadToEndAsync().ConfigureAwait(true);
+            }
+        }
+    }
+}
